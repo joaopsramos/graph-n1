97593c751407d38d403666b44cb89e10e4d2fded
João Ramos <joaopsramos231@gmail.com>
2022-09-26 09:39:27 -0300

    add edges on node display

--- src/node.rs
+++ src/node.rs
@@ -7,7 +7,7 @@ pub struct Node {
     pub code: usize,
     pub name: String,
     pub local_type: String,
-    pub edges: Vec<usize>
+    pub edges: Vec<usize>,
 }
 
 impl Node {
@@ -32,12 +32,32 @@ impl Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}Código: {}, Nome: {}, Tipo do local: {}{}",
+            "{}Código: {}, Nome: {}, Tipo do local: {}, Arestas: {}{}",
             "|".green(),
             self.code.to_string().cyan(),
             self.name.cyan(),
             self.local_type.cyan(),
+            format_edges(&self.edges),
             "|".green()
         )
     }
+}
+
+fn format_edges(edges: &Vec<usize>) -> String {
+    let mut iter = edges.iter().peekable();
+    let mut string = "[".to_string();
+
+    while let Some(edge) = iter.next() {
+        string = format!("{string}{}", edge.to_string().cyan());
+
+        if iter.peek().is_none() {
+            continue;
+        }
+
+        string.push_str(", ");
+    }
+
+    string.push_str("]");
+
+    string
 }
